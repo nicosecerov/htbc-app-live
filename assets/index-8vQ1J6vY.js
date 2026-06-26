a575275a1a4affdfac2328b4ea32fbeb212db8f7
Nicolas Secerov <nicolas.secerov@gmail.com>
2026-06-26 18:54:59 +0200

    Publish native OAuth ready web build

---
+++ assets/index-8vQ1J6vY.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-GDiSNVos.js","assets/index-Cr8qZ6oG.js","assets/index-Daib9lCP.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-Cr8qZ6oG.js";const _=p("App",{web:()=>r(()=>import("./web-GDiSNVos.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
