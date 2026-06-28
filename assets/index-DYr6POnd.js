6caf803130b0ca97cb562157c0aca54115ae89bf
Nicolas Secerov <nicolas.secerov@gmail.com>
2026-06-28 13:02:46 +0200

    Deploy HTBC build 14 rider cleanup

--- assets/index-DYr6POnd.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DvXExara.js","assets/index-Dio2xeK_.js","assets/index-DaaaxKou.css"])))=>i.map(i=>d[i]);
-import{r as e,_ as o}from"./index-Dio2xeK_.js";const _=e("Browser",{web:()=>o(()=>import("./web-DvXExara.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
