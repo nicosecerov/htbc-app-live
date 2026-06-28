c7f97560114dedf000b16ffa5ea4fad30a3a1bf9
Nicolas Secerov <nicolas.secerov@gmail.com>
2026-06-28 13:44:48 +0200

    Deploy HTBC build 18 radical UI purge

---
+++ assets/index-BUV21DvL.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BXjNlZYI.js","assets/index-D1ZwTDux.js","assets/index-BOFLug3r.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-D1ZwTDux.js";const _=p("App",{web:()=>r(()=>import("./web-BXjNlZYI.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
