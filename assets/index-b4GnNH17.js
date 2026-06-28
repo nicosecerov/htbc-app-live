6bf0726be1630bf0ecd4087de1b87507812096df
Nicolas Secerov <nicolas.secerov@gmail.com>
2026-06-28 13:36:45 +0200

    Deploy HTBC build 17 simple surface

---
+++ assets/index-b4GnNH17.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BBTJYFN-.js","assets/index-LumkxGFe.js","assets/index-BWfO5w9z.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-LumkxGFe.js";const _=p("App",{web:()=>r(()=>import("./web-BBTJYFN-.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
--- assets/index-zg1-4t6M.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-B1iHMp1-.js","assets/index-DDPGSzls.js","assets/index-Cyj7uXWK.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-DDPGSzls.js";const _=p("App",{web:()=>r(()=>import("./web-B1iHMp1-.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
