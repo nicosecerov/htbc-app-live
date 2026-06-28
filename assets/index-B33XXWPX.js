66b096ad44c550ee1ca77fe5c0e8ea6327d94fc0
Nicolas Secerov <nicolas.secerov@gmail.com>
2026-06-28 13:12:24 +0200

    Deploy HTBC build 15 release label

---
+++ assets/index-B33XXWPX.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BcrNhSbL.js","assets/index-Cl94KLik.js","assets/index-BQ4vfzKr.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-Cl94KLik.js";const _=e("Browser",{web:()=>o(()=>import("./web-BcrNhSbL.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
---
+++ assets/index-KGSG9Tzl.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-ad5NcuSk.js","assets/index-Cl94KLik.js","assets/index-BQ4vfzKr.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-Cl94KLik.js";const _=p("App",{web:()=>r(()=>import("./web-ad5NcuSk.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
