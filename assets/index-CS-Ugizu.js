e3ab2757b49b8fd95a69176911639453ca514038
Nicolas Secerov <nicolas.secerov@gmail.com>
2026-06-28 12:49:23 +0200

    Deploy HTBC build 13 simplify beta

---
+++ assets/index-CS-Ugizu.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DNZ46-Jo.js","assets/index-Dio2xeK_.js","assets/index-DaaaxKou.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-Dio2xeK_.js";const _=p("App",{web:()=>r(()=>import("./web-DNZ46-Jo.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
--- assets/index-CzNqUL5f.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BcSU19WD.js","assets/index-BzNBoerp.js","assets/index-CKrybxzz.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-BzNBoerp.js";const _=p("App",{web:()=>r(()=>import("./web-BcSU19WD.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
