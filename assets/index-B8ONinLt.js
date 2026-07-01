a527751b1b0debbca7432a524b9c423ef740c964
Nicolas Secerov <nicolas.secerov@gmail.com>
2026-07-02 01:55:48 +0200

    HTBC build51c : fix suppression signalements (resurrection offline), section cockpit Signalements actifs, page Actions allegee (Voir tout)

---
+++ assets/index-B8ONinLt.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BeetqDuj.js","assets/index-CwZK_qAq.js","assets/index-DYko5aN5.css"])))=>i.map(i=>d[i]);
+import{r as e,_ as o}from"./index-CwZK_qAq.js";const _=e("Browser",{web:()=>o(()=>import("./web-BeetqDuj.js"),__vite__mapDeps([0,1,2])).then(r=>new r.BrowserWeb)});export{_ as Browser};
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'htbc-terrain-build51-plan-resolution-4023397b';
+const CACHE_NAME = 'htbc-terrain-build51-plan-resolution-ab4607af';
 
 const PRECACHE_URLS = [
   './',
@@ -12,16 +12,16 @@ const PRECACHE_URLS = [
   // Les assets hashes du bundle (JS/CSS + htbc-config) sont injectes au build par
   // scripts/inject-sw-precache.mjs : sans eux, premiere ouverture hors ligne = ecran blanc.
   './htbc-config.js',
-  './assets/index-BpQvC2n9.js',
-  './assets/index-BrW9_MtO.js',
-  './assets/index-CEyPS2mA.js',
-  './assets/index-DRH7SixA.js',
+  './assets/index-B8ONinLt.js',
+  './assets/index-Baav5e-9.js',
+  './assets/index-CE7iRkXd.js',
+  './assets/index-CEO0u3Gy.js',
+  './assets/index-CwZK_qAq.js',
   './assets/index-DYko5aN5.css',
-  './assets/index-kIY_gAnz.js',
-  './assets/native-geo-DUs1V6kZ.js',
-  './assets/web-BIGa0bo0.js',
-  './assets/web-CacrZuAY.js',
-  './assets/web-Dw9Hr_k9.js',
+  './assets/native-geo-ChW0ZpZH.js',
+  './assets/web-BbpQyXDh.js',
+  './assets/web-BeetqDuj.js',
+  './assets/web-RniO4dGr.js',
 ];
 
 function scopedUrl(path) {
